1b313a65c6a0105e88e8e0927a729fa351a72d72
photodude <walt@waltsorensen.com>
2016-01-04 22:29:48 -0700

    Initial commit of step 1
    create required files

---
+++ source/administrator/components/com_fof3basic/sql/install.mysql.utf8.sql
@@ -0,0 +1,47 @@
+CREATE TABLE IF NOT EXISTS `#__fof3basic_examples` (
+	`fof3basic_example_id`         INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`fof3basic_category_id`        INT(11) UNSIGNED NOT NULL,
+	`title`                        VARCHAR(255)     NOT NULL,
+	`slug`                         VARCHAR(255) NOT NULL DEFAULT '',
+	`summary`                      TEXT             NOT NULL,
+	`link`                         VARCHAR(255)     NOT NULL,
+	`tags`                         TEXT             NOT NULL,
+	`viewlevel`                    INT(11)          NOT NULL,
+
+	`enabled`                      TINYINT(1)       NOT NULL DEFAULT '1',
+	`ordering`                     BIGINT(20) UNSIGNED NOT NULL,
+	`created_on`                   DATETIME         NOT NULL default '0000-00-00 00:00:00',
+	`created_by`                   INT(11)          NOT NULL DEFAULT 0,
+	`modified_on`                  DATETIME         NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`modified_by`                  INT(11)          NOT NULL DEFAULT 0,
+	`locked_on`                    DATETIME         NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`locked_by`                    INT(11)          NOT NULL DEFAULT 0,
+	`hits`                         BIGINT(20) UNSIGNED NOT NULL default '0',
+	PRIMARY KEY (`fof3basic_example_id`),
+	KEY `idx_locked` (`locked_by`),
+	KEY `idx_enabled` (`enabled`)
+)
+	ENGINE=InnoDB DEFAULT COLLATE = utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `#__fof3basic_categories` (
+	`fof3basic_category_id`   INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`title`                        VARCHAR(255)     NOT NULL,
+	`slug`                         VARCHAR(255) NOT NULL DEFAULT '',
+	`description`                  mediumtext       NOT NULL,
+	`tags`                         TEXT             NOT NULL,
+	`viewlevel`                    INT(11)          NOT NULL,
+
+	`enabled`                      TINYINT(1)       NOT NULL DEFAULT '1',
+	`ordering`                     BIGINT(20) UNSIGNED NOT NULL,
+	`created_on`                   DATETIME         NOT NULL default '0000-00-00 00:00:00',
+	`created_by`                   INT(11)          NOT NULL DEFAULT 0,
+	`modified_on`                  DATETIME         NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`modified_by`                  INT(11)          NOT NULL DEFAULT 0,
+	`locked_on`                    DATETIME         NOT NULL DEFAULT '0000-00-00 00:00:00',
+	`locked_by`                    INT(11)          NOT NULL DEFAULT 0,
+	`hits`                         BIGINT(20) UNSIGNED NOT NULL default '0',
+	PRIMARY KEY (`fof3basic_category_id`),
+	KEY `idx_locked` (`locked_by`),
+	KEY `idx_enabled` (`enabled`)
+)
+	ENGINE=InnoDB DEFAULT COLLATE = utf8_general_ci;
---
+++ source/administrator/components/com_fof3basic/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS `#__fof3basic_examples`;
+DROP TABLE IF EXISTS `#__fof3basic_categories`;
